bd1af7292a4a18ea590d178876835fc0bf6ed644
Eduardo Quirós <github@eduardoquiros.xyz>
2020-09-22 11:33:48 -0700

    Hacer lo mismo pero con Windows Forms Control Library

---
+++ WindowsFormsControl/UserControl1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace WindowsFormsControl
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Oprímeme = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(154, 122);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Oprímeme
+            // 
+            this.Oprímeme.Location = new System.Drawing.Point(167, 279);
+            this.Oprímeme.Name = "Oprímeme";
+            this.Oprímeme.Size = new System.Drawing.Size(75, 23);
+            this.Oprímeme.TabIndex = 1;
+            this.Oprímeme.Text = "Oprímeme";
+            this.Oprímeme.UseVisualStyleBackColor = true;
+            this.Oprímeme.Click += new System.EventHandler(this.Oprímeme_Click);
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Oprímeme);
+            this.Controls.Add(this.textBox1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(400, 400);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Oprímeme;
+    }
+}
